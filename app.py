e180b1b8aa4c31ca81a898bf7e0f8b34278f4c6f
asburyk <asburyk@rose-hulman.edu>
2024-11-17 16:59:37 -0500

    saving working

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import os
 import random
 import math
 from flask import Flask, flash, request, redirect, url_for
+import time
+import datetime
 
 database_file = "users.db"
 db = pickledb.load(database_file, True)
@@ -97,7 +99,36 @@ def getPuzzle(user, id):
 
 @app.post("/users/<user>/puzzle")
 def postPuzzle(user):
-    return 0
+    data = flask.request.json
+    i = 0
+    while (i < db.llen(db_name)):
+        if (db.lget(db_name, i)["username"] == data["username"]):
+            if (db.lget(db_name, i)["password"] == data["password"]):
+                break
+            else:
+                return flask.Response(response=json.dumps({"success": False}), status=200, headers = {"Content-Type": "application/json"})
+        i += 1
+    if (i >= db.llen(db_name)):
+        return flask.Response(response=json.dumps({"success": False}), status=200, headers = {"Content-Type": "application/json"})
+    userinfo = db.lget(db_name, i)
+    db.lpop(db_name, i)
+    if (userinfo.keys().__contains__("saved_games")):
+        id = len(userinfo["saved_games"])
+    else:
+        id = 0
+        userinfo["saved_games"] = []
+    timestamp = time.time()
+    timestamp = datetime.datetime.fromtimestamp(timestamp=timestamp).strftime('%Y-%m-%d %H:%M:%S') #https://stackoverflow.com/questions/13890935/does-pythons-time-time-return-the-local-or-utc-timestamp
+    newGame = {}
+    newGame["id"] = id
+    newGame["timestamp"] = timestamp
+    newGame["size"] = data["currentSize"]
+    newGame["init"] = data["currentMines"]
+    newGame["moves"] = data["currentMoves"]
+    userinfo["saved_games"].append(newGame)
+    db.ladd(db_name, userinfo)
+    return flask.Response(response=json.dumps({"success": True}), status=200, headers = {"Content-Type": "application/json"})
+    
 
 app.run(host='0.0.0.0', port=22009,debug=True)
 
